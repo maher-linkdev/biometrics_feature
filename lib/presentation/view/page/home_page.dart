0224b89b063096b53c69d8bfd6eb21081b1e9cba
Maher EL-Amary <maherelamary@gmail.com>
2025-04-10 12:46:26 +0200

    fix: align text to be centered

--- lib/presentation/view/page/home_page.dart
+++ lib/presentation/view/page/home_page.dart
@@ -10,35 +10,27 @@ class HomePage extends ConsumerWidget {
     final biometricsState = ref.watch(biometricsProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Biometrics Authentication'),
-      ),
+      appBar: AppBar(title: const Text('Biometrics Authentication')),
       body: Center(
-        child:
-             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Biometrics Status:',
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                const SizedBox(height: 16),
-                Text('Available: ${biometricsState.isAvailable}'),
-                Text('Enabled: ${biometricsState.isEnabled}'),
-                Text('Available Types: ${biometricsState.availableTypes.join(", ")}'),
-                const SizedBox(height: 32),
-                ElevatedButton(
-                  onPressed: biometricsState.isAvailable && biometricsState.isEnabled
-                      ? () =>  ref.read(biometricsProvider.notifier).authenticate(context)
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Biometrics Status:', style: Theme.of(context).textTheme.headlineSmall),
+            const SizedBox(height: 16),
+            Text('Available: ${biometricsState.isAvailable}'),
+            Text('Enabled: ${biometricsState.isEnabled}'),
+            Text('Available Types: ${biometricsState.availableTypes.join(", ")}', textAlign: TextAlign.center),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed:
+                  biometricsState.isAvailable && biometricsState.isEnabled
+                      ? () => ref.read(biometricsProvider.notifier).authenticate(context)
                       : null,
-                  child: const Text('Authenticate'),
-                ),
-              ],
+              child: const Text('Authenticate'),
             ),
-
-
+          ],
+        ),
       ),
     );
   }
 }
-
